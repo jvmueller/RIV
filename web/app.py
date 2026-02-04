d328b240c71997941a0e5c0ecabf77f2c67e4017
Neva Rognes <rognesn@umich.edu>
2026-02-04 17:41:55 -0500

    Added some pages and restyled

--- web/app.py
+++ web/app.py
@@ -38,5 +38,21 @@ def results():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/make-a-line')
+def mal():
+    return render_template('index.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
